cedbdf79eddc3b94ee1746421c02706158422d0a
beatriangu <beatriz.lamiquizdauden@gmail.com>
2024-10-09 18:13:16 +0200

    congender32

--- d05/ex09/admin.py
+++ d05/ex09/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Planets, People
 
-# Register your models here.
+admin.site.register(Planets)
+admin.site.register(People)
--- d05/ex09/urls.py
+++ d05/ex09/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
-from .views import Populate, Display, Update
-
-app_name = 'ex09'
+from .views import display
 
 urlpatterns = [
-    path('populate/', Populate.as_view(), name='populate'),
-    path('display/', Display.as_view(), name='display'),
-    path('update/', Update.as_view(), name='update'),
+    path('display/', display, name='display_view'),
 ]
